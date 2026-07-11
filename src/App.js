e9f1b3ca3bb1c0065fe23786adfd5b9fb103d08f
bahram12212 <mohammadibahram36@gmail.com>
2026-07-11 08:38:02 -0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Home from './Home/home';
+import  Home from './Home/home';
 import { Route, Routes } from "react-router-dom"
 import Skills from './pages/skills/skills';
 import { BrowserRouter } from 'react-router-dom';
